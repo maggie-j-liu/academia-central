ca48e8ae990d26a88c9025572118dbe7585d68e5
jliu24 <jliu24@mit.edu>
2021-07-25 11:02:48 -0400

    basic tags in profile & events section in main page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import useUser from "utils/firebase/useUser";
 import SimpleImageSlider from "react-simple-image-slider";
+import Link from "next/link";
+import formatDate from "utils/formatDate";
+import { db } from "utils/firebase/admin";
 
 const images = [
   {
@@ -23,21 +26,94 @@ const images = [
   },
 ];
 
-const Home = () => {
+const Home = ({ events }) => {
   return (
+
     <nav>
-      <div>Our hacktable project :)</div>
-      <div>
-        <SimpleImageSlider
-          width={896}
-          height={504}
-          slideDuration={0.05}
-          images={images}
-          showNavs={true}
-        />
+      <div className="w-3/4 max-w-3xl mx-auto my-8 text-center">
+        <div>Our hacktable project :)</div>
+        <div>
+          <SimpleImageSlider
+            width={896}
+            height={504}
+            slideDuration={0.05}
+            images={images}
+            showNavs={true}
+            showBullets={true}
+          />
+        </div>
       </div>
+
+
+      <div className={"w-full min-h-screen bg-gray-100 py-8"}>
+        <div className={"w-4/5 max-w-7xl mx-auto"}>
+          <h1 className={"text-4xl font-semibold text-center mb-6"}>Events</h1>
+          <div className={"w-full grid grid-cols-3 gap-8 "}>
+            {Object.entries(events).slice(0,3).map(([key, value]) => (
+              <Link key={key} href={`/events/${key}`}>
+                <a
+                  className={
+                    "bg-white rounded-md py-6 px-4 hover:shadow-xl transition-shadow block"
+                  }
+                >
+                  <img
+                    src={value.imageUrl}
+                    className={"rounded-md object-contain"}
+                    alt={`${value.eventName} Image`}
+                  />
+                  <div className={"mt-4"}>
+                    <h2 className={"text-xl font-medium"}>{value.eventName}</h2>
+                    <p>
+                      {formatDate(new Date(value.startDate))} -{" "}
+                      {formatDate(new Date(value.endDate))}
+                    </p>
+                    <p className={"text-gray-700"}>{value.description}</p>
+                  </div>
+                </a>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="w-3/4 max-w-3xl mx-auto my-8">
+          <div className={"text-xl font-semibold text-center mb-6"}>
+            <Link href={"/events"}>
+              <a
+                  className={
+                    "bg-white rounded-md py-6 px-4 hover:shadow-xl transition-shadow block"
+                  }
+                >
+              Explore more events
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+
     </nav>
   );
 };
+
+
+
+export async function getStaticProps() {
+  const events =
+    (await db
+      .ref("events")
+      .once("value")
+      .then((snapshot) => snapshot.val())) ?? {};
+  const filteredEvents = {};
+  for (const [key, { userId, ...rest }] of Object.entries(events)) {
+    filteredEvents[key] = rest;
+  }
+  return {
+    props: {
+      events: filteredEvents,
+    },
+    revalidate: 10,
+  };
+}
+
+
+
 
 export default Home;
