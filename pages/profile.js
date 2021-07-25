60ca05d9040ae53b768d27bdee880d93c9ac15e6
jliu24 <jliu24@mit.edu>
2021-07-25 03:44:15 -0400

    tags, but they don't save/load each time you enter profile

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,52 @@
-import Link from "next/link";
+
+import { useState, useRef } from "react";
+import initFirebase from "utils/firebase/setup";
+
+initFirebase();
+
 
 const profile = () => {
-  return <div>your profile</div>;
-};
+  const defaults = {
+    tags: [],
+  };
+  const [formData, setFormData] = useState(defaults);
 
+  const [currentTag, setCurrentTag] = useState("");
+
+  
+  const handleKeyDown = (e) => {
+    if (e.key == "Enter") {
+      e.preventDefault();
+      changeData("tags", [...formData.tags, e.target.value]);
+      setCurrentTag("");
+    }
+  };
+  return (
+    <div className={"w-3/4 max-w-3xl mx-auto my-8"}>
+      <div className={"flex flex-col"}>
+        <div className={"space-y-8"}>
+          <label className={"block"}>
+            <p>Tags</p>
+            <input
+              type={"text"}
+              placeholder={"Add tags that describe your interests"}
+              className={"form-input input-box w-full"}
+              value={currentTag}
+              onChange={(e) => setCurrentTag(e.target.value)}
+              onKeyDown={handleKeyDown}
+            />
+          </label>
+          <div className={"flex flex-wrap gap-2"}>
+            {formData.tags.map((tag) => (
+              <div className={"bg-gray-100 px-2 py-0.5"}>
+                <span>{tag}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
+  
 export default profile;
